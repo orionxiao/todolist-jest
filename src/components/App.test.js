9d9c20a3625cb8d5ae170439ee67ab782b8bdfe9
Orion Xiao <opx200@stern.nyu.edu>
2019-04-12 13:28:25 -0400

    testing form functionality

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -37,4 +37,45 @@ describe("App Component", () => {
             ).toEqual("Add")
         })
     })
+
+    describe("when creating a note", () => {
+        let testNote = "test note"
+
+        beforeEach(() => {
+            app.find("FormControl").simulate("change", {
+                target: {
+                    value: testNote
+                }
+            })
+        })
+
+        it("updates the text in state", () => {
+            expect(app.state().text).toEqual(testNote)
+        })
+
+        describe("when submitting a new note", () => {
+            beforeEach(() => {
+                app.find(".btn")
+                    .at(0)
+                    .simulate("click")
+            })
+
+            it("adds the new note to state", () => {
+                expect(app.state().notes[0].text).toEqual(testNote)
+            })
+
+            describe("when clicking the clear button after saving", () => {
+                beforeEach(() => {
+                    app.find(".btn")
+                        .at(1)
+                        .simulate("click")
+                })
+
+                it("clears the notes in state", () => {
+                    // console.log(app.state().notes)
+                    expect(app.state().notes.length).toEqual(0)
+                })
+            })
+        })
+    })
 })
